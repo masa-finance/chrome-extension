c6e77b7bd51e4bd1fcdee4f705b4b812dda77c10
hide-on-bush-x <hideonbushx123@gmail.com>
2024-01-23 10:40:00 -0300

    feat: sidebar

--- src/react/features/Dashboard/Sidebar.tsx
+++ src/react/features/Dashboard/Sidebar.tsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import { Column } from '../../components/Column';
+import { Row } from '../../components/Row';
+
 export const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <img src='/masalogo.png' width='100' />
+      <img src='/masalogo.png' className='sidebar-logo' width='100' />
+
+      <Column className='sidebar-item-container'>
+        <Row className='sidebar-item'><img src="/icons/dashboard-icon.png" alt="dashboard-icon"></img><p>Dashboard</p></Row>
+        <Row className='sidebar-item'><img src="/icons/referral-icon.png" alt="referral-icon"></img><p>Referrals</p></Row>
+      </Column>
     </div>
   )
 }
